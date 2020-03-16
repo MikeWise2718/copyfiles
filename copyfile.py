f47c14fd8954a21fa1250ac38ac5d009f39c620f
MikeWise2718 <mwise@microsoft.com>
2020-03-16 12:21:46 +0100

    more messaging and options

--- copyfile.py
+++ copyfile.py
@@ -2,10 +2,12 @@ import os
 import shutil
 import argparse
 import lgger as lgg
+import timeit
 
 parser = argparse.ArgumentParser(description='copyfile --sdir sourecdir --dir destdir')
 parser.add_argument('--sdir', type=str, default=".",   help='source directory',required=False)
 parser.add_argument('--ddir', type=str, default="dest", help='dest directory',required=False)
+parser.add_argument('--exec', type=bool, default=True, help='dest directory',required=False)
 
 args = parser.parse_args()
 
@@ -27,23 +29,39 @@ listfiles=[
     ]
 
 
-def copyFromTo(sdir,ddir):
+def copyFromTo(sdir,ddir,execute):
+    overwrittenfiles = 0
+    overwrittenbytes = 0
     for (fdname,fname) in listfiles:
         sfname = f"{sdir}/{fdname}{fname}"
         dfname = f"{ddir}/{fdname}{fname}"
         dddir = f"{ddir}/{fdname}"
-        fclr = lgg.cC
-        if not os.path.exists(dddir):
+        fclr = lgg.cC 
+        ovbytes = 0
+        if execute and (not os.path.exists(dddir)):
             lgg.info(f"Creating directory {dddir}",lgg.cR)
             os.makedirs(dddir)
-        elif os.path.exists(dfname):
-            fclr = lgg.cB
-        lgg.info(f"copy from {sfname} to {dfname}",fclr)
-        shutil.copyfile(sfname,dfname)
+        if os.path.exists(dfname):
+            fclr = lgg.cG
+            ftats = os.stat(dfname)
+            ovbytes = ftats.st_size
+            overwrittenbytes += ovbytes
+            overwrittenfiles += 1
+        execword = "execute" if execute else "fake"
+        lgg.info(f"{execword} copy from {sfname} to {dfname} overwrittenbytes:{ovbytes}",fclr)
+        if execute:
+            shutil.copyfile(sfname,dfname)
+    return (overwrittenfiles,overwrittenbytes)
 
 sdir = args.sdir
 ddir = args.ddir
+execute = args.exec
 
-lgg.info(f"Copying {len(listfiles)} files from {sdir} to {ddir}",lgg.cY)
+lgg.info(f"Copying {len(listfiles)} files from {sdir} to {ddir} execute:{execute}",lgg.cY)
 
-copyFromTo(args.sdir,args.ddir)
+start = timeit.timeit()
+(ovfiles,ovbytes) = copyFromTo(sdir,ddir,execute)
+elap = timeit.timeit()-start 
+
+exword = "" if execute else "Would have "
+lgg.info(f"{exword} Overwritten files:{ovfiles}  overwritenbytes:{ovbytes} secs:{round(elap,3)} ",lgg.cY)
